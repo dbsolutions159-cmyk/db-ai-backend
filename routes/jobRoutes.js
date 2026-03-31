ab6c99c526f4c1e0e09b7b6c44b9edc91c084420
Dharmesh <your-email@gmail.com>
2026-03-31 08:53:01 -0700

    fixed model path case sensitive issue

--- routes/jobRoutes.js
+++ routes/jobRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Job = require("../models/Job");
+const Job = require("../models/job");
 
 // 🔍 Search jobs
 router.get("/jobs", async (req, res) => {
--- services/saveJobs.js
+++ services/saveJobs.js
@@ -1,4 +1,4 @@
-const Job = require("../models/Job");
+const Job = require("../models/job");
 
 async function saveJobs(data) {
   try {
