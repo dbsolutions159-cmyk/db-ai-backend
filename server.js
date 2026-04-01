3e0e3467424a7b3cc93169596135e417b8633864
dbsolutions159-cmyk <dbsolutions159@gmail.com>
2026-04-01 07:02:15 -0700

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require("cors");
 const cron = require("node-cron");
 
 // 🔥 IMPORT JOB FETCHER
@@ -49,7 +48,7 @@ cron.schedule("0 */6 * * *", async () => {
 });
 
 // 🔹 Start Server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
